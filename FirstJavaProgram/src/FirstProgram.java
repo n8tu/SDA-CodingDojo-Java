6b159751b898792d718a4a076b738ffd160ce7e6
n8tu <faisalz.nj@gmail.com>
2022-07-17 13:41:32 +0300

    First Program Assignment

--- FirstJavaProgram/src/FirstProgram.java
+++ FirstJavaProgram/src/FirstProgram.java
@@ -2,6 +2,6 @@ public class FirstProgram {
     public static void main(String[] args) {
         System.out.println("My name is Faisal");
         System.out.println("I am 23 years old");
-        System.out.println("My hometown is Hafer-Albaten, KSA");
+        System.out.println("My hometown is Hafer Albaten, KSA");
     }
 }
