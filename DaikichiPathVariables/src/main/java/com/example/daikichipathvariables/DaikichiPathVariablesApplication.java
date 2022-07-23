a90b1bf7138e55d21c23a2c6a6b36c21356263b7
n8tu <faisalz.nj@gmail.com>
2022-07-23 19:59:45 +0300

    Daikichi Path Variables Assignment

---
+++ DaikichiPathVariables/src/main/java/com/example/daikichipathvariables/DaikichiPathVariablesApplication.java
@@ -0,0 +1,32 @@
+package com.example.daikichipathvariables;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DaikichiPathVariablesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaikichiPathVariablesApplication.class, args);
+    }
+
+    @RequestMapping("/daikichi/travel/{city}")
+    public String travel(@PathVariable("city") String city){
+        return String.format("Congratulations! You will soon travel to %s!",city);
+    }
+
+    @RequestMapping("/daikichi/lotto/{number}")
+    public String travel(@PathVariable("number") int number){
+        if(number % 2 == 0){
+            return "You will take a grand journey in the near future, but be weary of tempting offers";
+        }else{
+            return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+        }
+    }
+
+}
